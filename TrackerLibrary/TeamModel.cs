ad800fee9a87daea20a54d55438ba9550aad4c1e
CMRoss <ChrisRoss@u.boisestate.edu>
2021-01-19 19:32:07 -0700

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        //property holding one piece of data ("prop" tab tab)
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+
+    //Pre C# 6 (can initialize with "= List<Person>(); now)
+    //    public TeamModel()
+    //    {
+    //        TeamMembers = new List<PersonModel>();
+    //    }
+    }
+}
